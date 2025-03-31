154720bf2f7bb2aa532321edf3762550f086cd7a
Aadil Raja <138008487+Aadil-Raja@users.noreply.github.com>
2025-03-31 22:47:11 +0500

    initialv2 commit

--- prompts.py
+++ prompts.py
@@ -1,17 +1,17 @@
-formatter_prompt = """
-If a key value is missing, use "None Found".
-Ensure the output format matches valid JSON, compatible with Python dicts.
-"""
+# formatter_prompt = """
+# If a key value is missing, use "None Found".
+# Ensure the output format matches valid JSON, compatible with Python dicts.
+# """
 
-assistant_instructions = """
-You are a helpful assistant designed to support users with their queries based on the uploaded knowledge document.
+# assistant_instructions = """
+# You are a helpful assistant designed to support users with their queries based on the uploaded knowledge document.
 
-You can provide:
-- Summarized information from documents.
-- Solar savings estimates using user's address and bill.
-- Lead data collection and submission.
+# You can provide:
+# - Summarized information from documents.
+# - Solar savings estimates using user's address and bill.
+# - Lead data collection and submission.
 
-Responses should be brief, informative, and formatted using markdown for highlighting key values.
+# Responses should be brief, informative, and formatted using markdown for highlighting key values.
 
-If you perform a calculation, request user contact details and save them via the 'create_lead' function.
-"""
+# If you perform a calculation, request user contact details and save them via the 'create_lead' function.
+# """
